3bac032f11c268b71b018f1b603616143052f6a2
Kamoliddinov Sardor <111947544+KSardor@users.noreply.github.com>
2024-03-15 11:25:32 +0500

    added search and select for card

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
 
 const CardItem = () => {
-  return <div>CardItem</div>;
+  const [categories, setCategories] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setCategories(event.target.value);
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    // Bu yerda qidiruvni bajarish logikasi joylashadi
+    console.log(searchQuery);
+  };
+  return (
+    <div>
+      <div className="px-10 py-7">
+        <div className="flex items-center justify-between">
+          <div className="text-3xl font-bold">Все продукты</div>
+          <div>
+            <FormControl sx={{ m: 1, minWidth: 180 }} size="small">
+              <InputLabel id="demo-select-small-label">
+                Select a Category
+              </InputLabel>
+              <Select
+                labelId="demo-select-small-label"
+                id="demo-select-small"
+                value={categories}
+                onChange={handleChange}
+                autoWidth
+                label="Select a Category"
+              >
+                <MenuItem value={1}>electronics</MenuItem>
+                <MenuItem value={2}>jewelery</MenuItem>
+                <MenuItem value={3}>men's clothing</MenuItem>
+                <MenuItem value={4}>women's clothing</MenuItem>
+              </Select>
+            </FormControl>
+
+            <TextField
+              sx={{ m: 1, width: 180 }} // sx propertisi orqali uslubni sozlash
+              size="small"
+              variant="outlined"
+              label="Search"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleSearch} edge="end">
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardItem;
--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -1,9 +1,12 @@
+import CardItem from "../CardItem/CardItem";
 import Header from "../Header/Header";
 
 const MainCard = () => {
   return (
-    <div className="max-w-7xl h-full bg-white mx-auto my-3 rounded-2xl">
+    <div className="max-w-7xl h-full bg-white mx-auto mt-16 rounded-2xl">
       <Header />
+      <hr />
+      <CardItem />
     </div>
   );
 };
