5c170b0bc15d00a2ed60c2bac7e0f23af9f63c9f
Kamoliddinov Sardor <111947544+KSardor@users.noreply.github.com>
2024-03-15 07:54:45 +0500

    add tailwindcss and mui and mui/icons

---
+++ src/components/MainCard/MainCard.tsx
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+const MainCard = () => {
+  return (
+    <div className="max-w-7xl h-full bg-white mx-auto my-3 rounded-2xl">
+      <Header />
+    </div>
+  );
+};
+
+export default MainCard;
